4f1157c3354211e737f9c09dd504f28f8d0ad4e4
Kira <kawoods826@gmail.com>
2019-10-18 11:54:52 -0700

    final code cleanup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,42 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
-import CharacterCard from './components/CharacterCard';
-
-
-
+import "./App.css";
+import CharacterCard from "./components/CharacterCard";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [cardInfo, setCardInfo] = useState([]);
-    useEffect(() => {
-      axios
-        .get('https://swapi.co/api/people/')
-        .then(response => {
-          const peopleObject = response.data.results;
-          setCardInfo(peopleObject);
-          console.log(peopleObject);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        const peopleObject = response.data.results;
+        setCardInfo(peopleObject);
+        console.log(peopleObject);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <div>
+      <div className="cardContainer">
         {cardInfo.map(cardInfo => {
           return <CharacterCard cardInfo={cardInfo} key={cardInfo.name} />;
         })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "./CharacterCard.css"
+import "./CharacterCard.css";
 import { Card } from "@blueprintjs/core";
 
 export default function CharacterCard(props) {
-    return (
-        <Card>
-            <h3>{props.cardInfo.name}</h3>
-            <p>Gender: {props.cardInfo.gender}</p>
-            <p>Hair Color: {props.cardInfo.hair_color}</p>
-            <p>Height: {props.cardInfo.height}</p>
-            <p>Mass: {props.cardInfo.mass}</p>
-        </Card>
-      );
-  };
-  
+  return (
+    <Card className="individualCard">
+      <h3>{props.cardInfo.name}</h3>
+      <p>Gender: {props.cardInfo.gender}</p>
+      <p>Hair Color: {props.cardInfo.hair_color}</p>
+      <p>Height: {props.cardInfo.height}</p>
+      <p>Mass: {props.cardInfo.mass}</p>
+    </Card>
+  );
+}
